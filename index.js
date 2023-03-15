8110efe38dd85b5a91175f2003a1ed6ca78f2679
Subhasish Halder <79743346+subh839@users.noreply.github.com>
2023-03-15 17:31:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-const express =require("express")
-const jwt= require("jsonwebtoken");
-const app=express();
-const secretKey="secretKey";
-app.get("/",(req,res)=>{
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+const secretKey = "secretKey";
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "json",
+  });
+});
+
+app.post("/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "Abhi",
+    email: "golp@gmail.com",
+  };
+  jwt.sign({ user }, secretKey, { expiresIn: "300s" }, (err, token) => {
     res.json({
-        message:"json"
-    })
-})
+      token,
+    });
+  });
+});
 
-app.post("/login",(req,res)=>{
-const user={
-    id:1,
-    username:"Abhi",
-    email:"golp@gmail.com"
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    console.log(token);
+    req.token = token;
+    next();
+  } else {
+    res.send({
+      result: "Invalid token",
+    });
+  }
 }
-jwt.sign({user},secretKey,{expiresIn:"300s"},(err,token)=>{
-res.json({
-    token
-})
-})
-})
 
-app.post("/profile",verifyToken,(req,res)=>{
-jwt.verify(req.token,secretKey,(err,authData)=>{
-if(err){
-    res.send({result:"Invalid"})
-}else{
-    res.json({
-        message:"profile accessed",
-        authData
-    })
-}
-}) 
-    })
-
-function verifyToken(req,res,next) {
-    const bearerHeader= req.headers['authorization'];
-    if(typeof bearerHeader !== 'undefined'){
-const bearer= bearerHeader.split(" ");
-const token= bearer[1];
-console.log(token);
-req.token=token;
-next();
-
-    }else{
-        res.send({
-            result:"Invalid token "
-        })
+app.post("/profile", verifyToken, (req, res) => {
+  jwt.verify(req.token, secretKey, (err, authData) => {
+    if (err) {
+      res.send({ result: "Invalid" });
+    } else {
+      res.json({
+        message: "profile accessed",
+        authData,
+      });
     }
-}
+  });
+});
 
-app.listen(5000,()=>{
-    console.log("running on 5000");
-})
+app.listen(5000, () => {
+  console.log("running on 5000");
+});
